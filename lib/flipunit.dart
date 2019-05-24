9adb1d7bf516acc50071096d7bf06e0495a4d54e
Mahasak Pijittum <mahasak@gmail.com>
2019-05-24 22:51:05 +0800

    Add flipdirection

--- lib/flipunit.dart
+++ lib/flipunit.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'dart:math' as math;
 import 'dart:async';
 
+enum FlipDirection { up, down }
+
 class FlipWidget extends StatelessWidget {
   Widget child;
   final int digit;
@@ -11,8 +13,28 @@ class FlipWidget extends StatelessWidget {
 
   String padZero(int num) => (num < 10) ? "0$num" : "$num";
 
+  String calculatePrevious(digit, FlipDirection direction, triggerValue) {
+    int previousDigit = (direction == FlipDirection.up)
+        ? digit - 1
+        : (digit == 0) ? 0 : digit + 1;
+
+    previousDigit = (direction == FlipDirection.down && previousDigit == 60)
+        ? 0
+        : previousDigit;
+
+    return (direction == FlipDirection.down)
+        ? padZero(previousDigit)
+        : padZero((previousDigit == -1) ? triggerValue - 1 : previousDigit);
+  }
+
   @override
   Widget build(BuildContext context) {
+    Widget digit1 = Text(
+      padZero(digit),
+      style: TextStyle(
+          fontWeight: FontWeight.bold, fontSize: 80.0, color: Colors.white),
+    );
+
     return Container(
         alignment: Alignment.center,
         width: 135.0,
@@ -26,13 +48,7 @@ class FlipWidget extends StatelessWidget {
               child: Align(
             alignment: Alignment.topCenter,
             heightFactor: 0.5,
-            child: Text(
-              padZero(digit),
-              style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 80.0,
-                  color: Colors.white),
-            ),
+            child: digit1,
           )),
           Padding(
             padding: EdgeInsets.only(top: 2.0),
@@ -41,54 +57,8 @@ class FlipWidget extends StatelessWidget {
               child: Align(
             alignment: Alignment.bottomCenter,
             heightFactor: 0.5,
-            child: Text(
-              padZero(digit),
-              style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 80.0,
-                  color: Colors.white),
-            ),
+            child: digit1,
           )),
-        ]));
-  }
-
-  Widget build2(BuildContext ctx) {
-    return Container(
-        alignment: Alignment.center,
-        width: 96.0,
-        height: 128.0,
-        child: Row(mainAxisSize: MainAxisSize.min, children: [
-          Container(
-              alignment: Alignment.center,
-              width: 96.0,
-              height: 128.0,
-              decoration: BoxDecoration(
-                color: Colors.black,
-                borderRadius: BorderRadius.all(Radius.circular(4.0)),
-              ),
-              child: ClipRect(
-                  child: Align(
-                alignment: Alignment.topCenter,
-                heightFactor: 0.5,
-                child: child,
-              ))),
-          Padding(
-            padding: EdgeInsets.only(top: 2.0),
-          ),
-          Container(
-              alignment: Alignment.center,
-              width: 96.0,
-              height: 128.0,
-              decoration: BoxDecoration(
-                color: Colors.black,
-                borderRadius: BorderRadius.all(Radius.circular(4.0)),
-              ),
-              child: ClipRect(
-                  child: Align(
-                alignment: Alignment.bottomCenter,
-                heightFactor: 0.5,
-                child: child,
-              )))
         ]));
   }
 }
