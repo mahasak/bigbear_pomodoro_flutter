f0a38cf9379ffc1428fc966771f4448d018a1b1c
Mahasak Pijittum <mahasak@gmail.com>
2019-05-26 21:46:56 +0800

    change main to flip widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:bigbear_pomodoro/flipunit.dart';
+import 'package:bigbear_pomodoro/flipwidget.dart' as prefix0;
 import 'package:flutter/material.dart';
 
 import 'flipwidget.dart';
@@ -22,14 +23,30 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatelessWidget {
   final String title;
 
+  final bool debugMode = true;
+  DateTime now = DateTime.now();
+  DateTime dDay = DateTime(2018, 11, 26, 0, 0, 0);
+
   MyHomePage({this.title});
 
   @override
   Widget build(BuildContext context) {
+    dDay = DateTime(now.year, now.month, now.day, now.hour, now.minute+10, now.second);
+    
+    Duration _duration = dDay.difference(now);
+
     return new Scaffold(
         appBar: new AppBar(
           title: new Text(title),
         ),
-        body: new Center(child: FlipUnit<int>()));
+        body: new Center(child: FlipClock.reverseCountdown(
+            duration: _duration,
+            flipDirection: prefix0.FlipDirection.down,
+            digitColor: Colors.white,
+            backgroundColor: Colors.black,
+            digitSize: 30.0,
+            borderRadius: const BorderRadius.all(Radius.circular(3.0)),
+            //onDone: () => print('ih'),
+          )));
   }
 }
